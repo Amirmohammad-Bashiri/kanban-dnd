7c3d01ce9563ab9d202b87ad2b49c2a7d73cba68
Amirmohammad Bashiri <amirmohamad.bashiri@gmail.com>
2023-10-08 11:43:07 +0330

    Fixed edit mode bug

--- src/components/ColumnItem.tsx
+++ src/components/ColumnItem.tsx
@@ -48,12 +48,13 @@ function ColumnItem({
       style={style}
       {...attributes}
       {...listeners}
-      onClick={() => setEditMode(true)}
       className={clsx(
         "touch-none bg-columnBackgroundColor w-[350px] h-[500px] max-h-[500px] rounded-md flex flex-col",
         isDragging && "border-2 border-rose-500 opacity-40"
       )}>
-      <div className="bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between">
+      <div
+        onClick={() => setEditMode(true)}
+        className="bg-mainBackgroundColor text-md h-[60px] cursor-grab rounded rounded-b-none p-3 font-bold border-columnBackgroundColor border-4 flex items-center justify-between">
         <div className="flex gap-2">
           <div className="flex items-center justify-center px-2 py-1 text-sm rounded-full bg-columnBackgroundColor">
             0
